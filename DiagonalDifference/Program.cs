f597dd09fa2f11d4d9d7dcb45adb1bb0693978ec
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2021-01-07 22:36:26 +0000

    Add Diagonal Difference Problem

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] rowData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            int diagonalSum = 0;
+            int secondDiagonalSum = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    int number = matrix[row, col];
+
+                    if (row == col)
+                    {
+                        diagonalSum += number;
+                    }
+
+                    if (col == n - 1 - row)
+                    {
+                        secondDiagonalSum += number;
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(diagonalSum - secondDiagonalSum));
+        }
+    }
+}
