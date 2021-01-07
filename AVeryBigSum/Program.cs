7ba2e01e0946d49c0d93661caf2fef469188d28b
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2021-01-07 22:34:42 +0000

    Add A Very Big Sum Problem

---
+++ AVeryBigSum/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace AVeryBigSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int arrCount = int.Parse(Console.ReadLine());
+
+            long[] arr = Console.ReadLine()
+                .Split(' ')
+                .Select(long.Parse)
+                .ToArray();
+
+            long result = BigSum(arr, arrCount);
+
+            Console.WriteLine(result);
+        }
+
+        public static long BigSum(long[] arr, int arrCount)
+        {
+            long result = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                result += arr[i];
+            }
+
+            return result;
+        }
+    }
+}
