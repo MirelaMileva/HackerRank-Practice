73ae2a0860534e90e013ac294e0c5b0200c1b229
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2021-01-07 22:37:31 +0000

    Add Plus Minus Problem

---
+++ PlusMinus/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace PlusMinus
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int[] arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            decimal p = 0M;
+            decimal n = 0M;
+            decimal z = 0M;
+
+            for (int i = 0; i < number; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    p++;
+                }
+                else if (arr[i] == 0)
+                {
+                    z++;
+                }
+                else if (arr[i] < 0)
+                {
+                    n++;
+                }
+            }
+
+            decimal pos = p / number;
+            decimal neg = n / number;
+            decimal zeros = z / number;
+
+            Console.WriteLine($"{pos:f6}");
+            Console.WriteLine($"{neg:f6}");
+            Console.WriteLine($"{zeros:f6}");
+        }
+    }
+}
