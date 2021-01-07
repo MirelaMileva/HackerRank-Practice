d4cd7bc09f1ffc71d8071daaf1d2ede112cb5037
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2021-01-07 22:32:34 +0000

    Add Staircase problem

---
+++ Staircase/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Staircase
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            staircase(num);
+        }
+
+        public static void staircase(int num)
+        {
+            int col = 1;
+
+            for (int row = 0; row < num; row++)
+            {
+                Console.WriteLine(new string(' ', num - row - 1) + new string('#', col));
+
+                col++;
+            }
+        }
+    }
+}
