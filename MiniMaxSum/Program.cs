bce30d8c1cb01e8755cd38df1f178d579ece8d03
Mirela Mileva <mirela.tsvetanova2@gmail.com>
2021-01-07 23:10:41 +0000

    Add Min and Max Sum Problem

---
+++ MiniMaxSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace MiniMaxSum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            ;
+
+            miniMaxSum(arr);
+        }
+
+        static void miniMaxSum(int[] arr)
+        {
+            Array.Sort(arr);
+
+            long maxSum = 0;
+            long minSum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (i > 0)
+                {
+                    maxSum += arr[i];
+                }
+
+                if (i < 4)
+                {
+                    minSum += arr[i];
+                }
+
+            }
+
+            Console.WriteLine($"{minSum} {maxSum}");
+        }
+    }
+}
